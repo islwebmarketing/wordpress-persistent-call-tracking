a316aef31d2c3abcc033e2490bf85d57b67664ea
rpancura <rpancura@isl.ca>
2013-11-19 15:12:19 -0400

    Update phone_numbers.php
    Added utm parameters to about us link.

--- views/phone_numbers.php
+++ views/phone_numbers.php
@@ -7,7 +7,8 @@
   <!-- ISL Header -->
   <div style="margin-bottom: 15px; background-color: #ddd; border: 1px solid grey; padding: 5px;">
     <a href="http://www.isl.ca" target="_blank"><img src="<?php echo MY_BASE_URL ?>images/isl.png" alt="isl" style="padding-right: 10px;" /></a>
-    Thanks for using our plugin! If you'd like to know more about us, check out <a href="http://www.isl.ca" target="_blank">www.isl.ca</a>.
+    Thanks for using our plugin! If you'd like to know more about us, check out <a href="http://www.isl.ca?utm_source=wordpress+plugin&utm_medium=plugin&utm_content=persistent+call+tracking&utm_campaign=wordpress+plugin
+" target="_blank">www.isl.ca</a>.
   </div>
   <!-- END ISL Header -->
 
